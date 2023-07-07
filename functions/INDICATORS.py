f5879acf2f74688eab32871f0eef2535b1d949e1
jon <jcianci12@gmail.com>
2023-07-07 06:52:42 +0000

    getindicator data back!

--- functions/INDICATORS.py
+++ functions/INDICATORS.py
@@ -1,4 +1,7 @@
 # List of symbols for technical indicators
+import pandas as pd
+
+
 INDICATORS = [
     "RSI",
     "MACD",
@@ -14,8 +17,35 @@ INDICATORS = [
     "VORTEX",
 ]
 
-# %%
-"""
-Next we pull the historical data using yfinance
-Rename the column names because finta uses the lowercase names
-"""
+
+
+def _get_indicator_data(data):
+    """
+    Function that uses the finta API to calculate technical indicators used as the features
+    :return:
+    """
+
+    for indicator in INDICATORS:
+        ind_data = eval('TA.' + indicator + '(data)')
+        if not isinstance(ind_data, pd.DataFrame):
+            ind_data = ind_data.to_frame()
+        data = data.merge(ind_data, left_index=True, right_index=True)
+    data.rename(columns={"14 period EMV.": '14 period EMV'}, inplace=True)
+
+    # Also calculate moving averages for features
+    data['ema50'] = data['close'] / data['close'].ewm(50).mean()
+    data['ema21'] = data['close'] / data['close'].ewm(21).mean()
+    data['ema15'] = data['close'] / data['close'].ewm(14).mean()
+    data['ema5'] = data['close'] / data['close'].ewm(5).mean()
+
+    # Instead of using the actual volume value (which changes over time), we normalize it with a moving volume average
+    data['normVol'] = data['volume'] / data['volume'].ewm(5).mean()
+
+    # Remove columns that won't be used as features
+    del (data['open'])
+    del (data['high'])
+    del (data['low'])
+    del (data['volume'])
+    del (data['Adj Close'])
+    
+    return data
