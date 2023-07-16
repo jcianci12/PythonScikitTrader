1720cbc8870530cd14502a2ed6c6c9b607f154ca
Jonathan Cianci <jcianci12@gmail.com>
2023-07-16 09:35:14 +0000

    decimal bug fixed.

--- generateTPandSL.py
+++ generateTPandSL.py
@@ -31,7 +31,7 @@ def calculate_prices(entry_price, ohlc=[]):
     print(atr)
 
     # Calculate take profit and stop loss prices
-    takeprofitprice = entry_price + 3 * decimal.Decimal(atr[-1])
+    takeprofitprice = decimal.Decimal(entry_price) + 3 * decimal.Decimal(atr[-1])
     stoplossprice = entry_price + 3 * decimal.Decimal(atr[-1])
 
     return takeprofitprice, stoplossprice
